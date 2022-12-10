22241b6cf122a0f1797200046ea7a62d51a312b3
m0nster.mind <mind@m0nster.io>
2022-12-10 18:59:01 +0300

    RequestCharacterNameCreatableDto: remove useless name range validation, pattern already contains range check; fix name pattern

--- finex-server-world-lineage2/src/main/java/ru/finex/ws/l2/network/model/dto/RequestCharacterNameCreatableDto.java
+++ finex-server-world-lineage2/src/main/java/ru/finex/ws/l2/network/model/dto/RequestCharacterNameCreatableDto.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 import ru.finex.core.network.NetworkCommandScoped;
 import ru.finex.network.netty.model.NetworkDto;
 
@@ -21,8 +20,7 @@ import ru.finex.network.netty.model.NetworkDto;
 public class RequestCharacterNameCreatableDto implements NetworkDto {
 
     @NotNull
-    @Length(min = 1, max = 16)
-    @Pattern(regexp = "[\\w\\d]{1,16}")
+    @Pattern(regexp = "^[\\w\\d]{3,16}$")
     private String name;
 
 }
