58a4f253c781b30dd01a07d7c9e06fc525b1071e
m0nster.mind <mind@m0nster.io>
2022-12-10 05:36:52 +0300

    More flexible starter class validation

--- finex-server-world-lineage2/src/main/java/ru/finex/ws/l2/validation/impl/StarterClassValidator.java
+++ finex-server-world-lineage2/src/main/java/ru/finex/ws/l2/validation/impl/StarterClassValidator.java
@@ -2,7 +2,6 @@ package ru.finex.ws.l2.validation.impl;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import org.apache.commons.lang3.ArrayUtils;
 import ru.finex.ws.l2.model.ClassId;
 import ru.finex.ws.l2.validation.ValidStarterClass;
 
@@ -11,25 +10,15 @@ import ru.finex.ws.l2.validation.ValidStarterClass;
  */
 public class StarterClassValidator implements ConstraintValidator<ValidStarterClass, Integer> {
 
-    private static final int[] STARTER_CLASSES = {
-        ClassId.HUMAN_FIGHTER.getId(),
-        ClassId.ElvenFighter.getId(),
-        ClassId.DarkFighter.getId(),
-        ClassId.OrcFighter.getId(),
-        ClassId.DwarvenFighter.getId(),
-        ClassId.MALE_SOLDIER.getId(),
-        ClassId.FEMALE_SOLDIER.getId(),
-        ClassId.ERTHEIA_FIGHTER.getId(),
-        ClassId.HumanWizard.getId(),
-        ClassId.ElvenWizard.getId(),
-        ClassId.DarkWizard.getId(),
-        ClassId.OrcMystic.getId(),
-        ClassId.DwarvenFighter.getId(),
-        ClassId.ERTHEIA_WIZARD.getId()
-    };
-
     @Override
     public boolean isValid(Integer value, ConstraintValidatorContext context) {
-        return ArrayUtils.contains(STARTER_CLASSES, value);
+        ClassId classId;
+        try {
+            classId = ClassId.ofId(value);
+        } catch (Exception e) {
+            return false;
+        }
+
+        return classId.getParent() == null;
     }
 }
